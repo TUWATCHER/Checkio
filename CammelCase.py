f84fccd9affa85ed2227c928cc301485c2cd50ea
Ilya <tuwatcher@gmail.com>
2016-06-12 14:03:20 +0300

    Update CammelCase.py

--- CammelCase.py
+++ CammelCase.py
@@ -1,4 +1,4 @@
 string = "my_first_string"
-string = [item.capitalize() for item in string.split('_')]
-new_string = ''.join(string)
+string = [item.capitalize() for item in string.split('_')] # Конструктор + трим строки
+new_string = ''.join(string) # Объединение строки
 print(new_string)
