7cdcb36b4234adfe5db391fbf622e716cc4eb96e
Ilya <tuwatcher@gmail.com>
2016-06-23 19:03:16 +0300

    Add files via upload

---
+++ Random_File.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+
+namespace Random_File
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           System.Diagnostics.Process.Start(getRandomFile(@"F:\other\torrent"));
+        }
+
+        private static string getRandomFile(string path)
+        {
+            try
+            {
+                var extensions = new string[] { ".torrent" };
+
+                var di = new DirectoryInfo(path);
+                return (di.GetFiles("*.*")
+                                    .Where(f => extensions.Contains(f.Extension.ToLower()))
+                                    .OrderBy(f => Guid.NewGuid())
+                                    .First()).FullName;
+            }
+            catch { return ""; }
+        }
+    }
+}
---
+++ Random_Folder.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+namespace Random_Folder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            DirectoryInfo[] subDirs; //Directory Array
+            DirectoryInfo root = new DirectoryInfo(@"F:\Music"); //Directory path
+            subDirs = root.GetDirectories(); //Get Directories
+            Random random = new Random();
+            int directory = random.Next(subDirs.Length); //Random index of Directory Array
+            DirectoryInfo randomDirectory = subDirs[directory]; //Get Random Directory
+            string path = @"F:\Music\" + randomDirectory.ToString(); //Random Directory Path
+            System.Diagnostics.Process.Start(path); //Open Folder
+        }
+    }
+}
